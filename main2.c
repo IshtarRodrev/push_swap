0aebcda69daa85e31fdb23166ac3594d09795cef
Alina Kechedzhy <akechedz@3-F-4.42heilbronn.de>
2026-01-12 19:18:19 +0100

    resolve my mains

---
+++ main2.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main2.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akechedz <akechedz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/23 20:26:05 by akechedz          #+#    #+#             */
+/*   Updated: 2026/01/12 19:14:08 by akechedz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_stack *A = NULL;
+t_stack *B = NULL;
+
+/* 
+# include <stdlib.h>
+
+
+int main()
+{
+  t_node value = {123, NULL, NULL};
+  t_node *head = NULL;
+  t_node *tmp = NULL;
+
+  head = calloc(1, sizeof(t_node));
+  head->data = 10;
+  tmp = calloc(1, sizeof(t_node));
+  tmp->data = 20;
+  
+  head->next = tmp;
+  tmp->prev = head;
+  
+  t_stack st;
+  
+  st.first = head;
+  st.last = tmp;
+  
+  swap(&st);
+}
+
+int main2()
+{
+  t_node value = {123, NULL, NULL};
+  value.data = 19;
+
+  t_node *n1 = NULL;
+  t_node *n2 = NULL;
+  t_node *n3 = NULL;
+
+  n1 = calloc(1, sizeof(t_node));
+  n1->data = 10;
+  n2 = calloc(1, sizeof(t_node));
+  n2->data = 20;
+  n3 = calloc(1, sizeof(t_node));
+  n3->data = 30;
+  
+  n1->next = n2;
+  n2->prev = n1;
+  
+  n2->next = n3;
+  n3->prev = n2;
+  
+  t_stack st;
+  
+  st.first = n1;
+  st.last = n3;
+  
+  swap(&st);
+}
+*/
